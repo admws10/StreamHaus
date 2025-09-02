a20eb17d90014c545e33484c1db3d657a49f6955
admws10 <bitomsky.adam@gmail.com>
2025-09-02 20:09:48 +0200

    Create parser.py

---
+++ plugin.video.streamhausaddon/resources/lib/parser.py
@@ -0,0 +1,7 @@
+def get_stream_url(title):
+    """
+    Zde vložíš logiku pro získání přímého odkazu z Prehrajto.
+    Pro testování můžeš vrátit veřejný MP4 odkaz.
+    """
+    # testovací video
+    return "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
