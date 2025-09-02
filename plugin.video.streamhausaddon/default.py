926e9f08046e42f121ba6f42192efa5f8a65ddb1
admws10 <bitomsky.adam@gmail.com>
2025-09-02 20:25:10 +0200

    Update default.py

--- plugin.video.streamhausaddon/default.py
+++ plugin.video.streamhausaddon/default.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import urllib.parse
 import xbmcplugin
 import xbmcgui
@@ -7,21 +8,56 @@ from resources.lib import parser
 handle = int(sys.argv[1])
 params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
 
-def search_movies():
-    # input box pro zadání názvu filmu
-    keyboard = xbmcgui.Dialog().input("Zadej název filmu")
-    if not keyboard:
-        return
+# cesta k historii
+HISTORY_FILE = os.path.join(os.path.dirname(__file__), "resources", "lib", "history.txt")
 
-    # volání parseru pro vyhledání filmů
+def load_history():
+    if os.path.exists(HISTORY_FILE):
+        with open(HISTORY_FILE, "r", encoding="utf-8") as f:
+            return [line.strip() for line in f.readlines()]
+    return []
+
+def save_history(query):
+    history = load_history()
+    if query in history:
+        history.remove(query)
+    history.insert(0, query)
+    history = history[:10]  # maximálně 10 položek
+    with open(HISTORY_FILE, "w", encoding="utf-8") as f:
+        f.write("\n".join(history))
+
+def search_movies():
+    # nabídka historie
+    history = load_history()
+    if history:
+        choice = xbmcgui.Dialog().select("Vyber z historie nebo napiš nový název", history + ["Napsat nový název"])
+        if choice == -1:
+            return
+        elif choice < len(history):
+            keyboard = history[choice]
+        else:
+            keyboard = xbmcgui.Dialog().input("Zadej název filmu")
+            if not keyboard:
+                return
+    else:
+        keyboard = xbmcgui.Dialog().input("Zadej název filmu")
+        if not keyboard:
+            return
+
+    save_history(keyboard)
+
     movies = parser.search_movies(keyboard)
     if not movies:
         xbmcgui.Dialog().notification("Info", "Žádné filmy nenalezeny", xbmcgui.NOTIFICATION_INFO)
         return
 
     for movie in movies:
-        url = f"?action=play&title={urllib.parse.quote(movie['title'])}&link={urllib.parse.quote(movie['url'])}"
         li = xbmcgui.ListItem(label=movie['title'])
+        # mini náhled z Prehrajto (pokud existuje)
+        thumb = parser.get_thumbnail(movie['url'])
+        if thumb:
+            li.setArt({"thumb": thumb, "icon": thumb, "fanart": thumb})
+        url = f"?action=play&title={urllib.parse.quote(movie['title'])}&link={urllib.parse.quote(movie['url'])}"
         xbmcplugin.addDirectoryItem(handle, url, li, isFolder=False)
     xbmcplugin.endOfDirectory(handle)
 
@@ -33,7 +69,6 @@ def play_movie(title, link):
     else:
         xbmcgui.Dialog().notification("Chyba", "Video nelze přehrát", xbmcgui.NOTIFICATION_ERROR)
 
-# rozhodnutí podle parametrů
 if "action" in params:
     if params["action"] == "play":
         play_movie(params["title"], params["link"])
