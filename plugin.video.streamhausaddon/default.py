55747937235825c3c3cfc90f13d0b363c45bb1a4
admws10 <bitomsky.adam@gmail.com>
2025-09-02 20:12:45 +0200

    Update default.py

--- plugin.video.streamhausaddon/default.py
+++ plugin.video.streamhausaddon/default.py
@@ -6,26 +6,30 @@ from resources.lib import parser
 
 handle = int(sys.argv[1])
 
-# zpracování parametrů z Kodi
-params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
+def search_movies():
+    # otevře dialog pro zadání názvu filmu
+    keyboard = xbmcgui.Dialog().input("Zadej název filmu")
+    if not keyboard:
+        return
 
-def list_movies():
-    # seznam testovacích filmů (nahraď logikou Prehrajto)
-    movies = ["Film A", "Film B", "Film C"]
-    for title in movies:
-        url = f"?action=play&title={urllib.parse.quote(title)}"
-        li = xbmcgui.ListItem(label=title)
+    # získá seznam filmů podle parseru
+    movies = parser.search_movies(keyboard)
+    for movie in movies:
+        url = f"?action=play&title={urllib.parse.quote(movie['title'])}&link={urllib.parse.quote(movie['url'])}"
+        li = xbmcgui.ListItem(label=movie['title'])
         xbmcplugin.addDirectoryItem(handle, url, li, isFolder=False)
     xbmcplugin.endOfDirectory(handle)
 
-def play_movie(title):
-    # tady zavoláš parser pro Prehrajto a získáš přímý odkaz na stream
-    url = parser.get_stream_url(title)
+def play_movie(title, link):
+    # zavolá parser pro přímý stream
+    url = parser.get_stream_url_from_page(link)
     xbmc.Player().play(url)
 
-# rozhodnutí podle parametrů
+# zpracování parametrů
+params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
+
 if "action" in params:
     if params["action"] == "play":
-        play_movie(params["title"])
+        play_movie(params["title"], params["link"])
 else:
-    list_movies()
+    search_movies()
