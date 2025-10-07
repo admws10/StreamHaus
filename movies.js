07dfffd3e2475d0a62241d26b6db6a43e683c671
StreamHaus App <app@streamhaus.com>
2025-10-07 19:58:29 +0200

    Add/Update movie: Spider-Man 3

---
+++ movies.js
@@ -0,0 +1,27 @@
+{
+  "movies": [
+    {
+      "id": "1759562455022",
+      "movieId": 559,
+      "title": "Spider-Man 3",
+      "streams": [
+        {
+          "id": "1",
+          "language": "English",
+          "quality": "1080p",
+          "url": "https://storage3-1.premiumcdn.net/30047836/Begt3nu7oHZkiDWs9GH2zulVH04HkmU1xUeQsG2HNmTrgSNjF9niaNI28s1rhX1sg4T6rt68CDBiRkUMEUN1sz6FVWhpdCbWc0rt1ZCq5fm2UeXh1qHqA.mp4?token=A1206ZyNVCax&expires=1759762554&sparams=token%2Cpath%2Cexpires&signature=8b7106b3a62f6fd9a17818621c794474289108fc",
+          "subtitles": [
+            {
+              "language": "Czech",
+              "url": "blob:https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--96435430.local-credentialless.webcontainer-api.io/be004a4a-16ad-48e4-b4e0-e0c57178b81f"
+            }
+          ]
+        }
+      ],
+      "addedBy": "admws10",
+      "addedAt": "2025-10-07T17:58:26.918Z"
+    }
+  ],
+  "lastUpdated": "2025-10-07T17:58:27.199Z",
+  "totalMovies": 1
+}
